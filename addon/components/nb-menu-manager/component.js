e45cb60279efc5b452076acc544135c778d0a8af
Doug <douglas.b.hill@gmail.com>
2018-03-03 14:07:16 -0800

    x

--- addon/components/nb-menu-manager/component.js
+++ addon/components/nb-menu-manager/component.js
@@ -65,7 +65,7 @@ export default Component.extend(InboundAction, {
         }
       }
       catch ( e ) {
-        console.log(e);
+      //  console.log(e);
       }
       //   element.remove();
     },
